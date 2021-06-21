0d6fbad2f68a7a996258c17aafd8e01dbab1a60f
Carlos <Carlos@xtrene.com>
2021-06-21 19:25:19 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tarea04_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int precio = -1;
+            string formaPago = "";
+            string cuenta = "";
+            do
+            {
+                Console.WriteLine("Introduce el precio del producto: ");
+                precio = Convert.ToInt32(Console.ReadLine());
+            }
+            while (precio <= 0);
+
+            do
+            {
+                Console.WriteLine("Introduce la forma de pago (efectivo o tarjeta): ");
+                formaPago = Console.ReadLine();
+            }
+            while (formaPago != "efectivo" && formaPago != "tarjeta");
+
+            if (formaPago == "tarjeta")
+            {
+                Console.WriteLine("Introduzca su número de cuenta: ");
+                cuenta = Console.ReadLine();
+                Console.WriteLine("Se ha realizado un cargo de {0} euros en su cuenta {1}", precio, cuenta);
+            }
+            else
+            {
+                Console.WriteLine("Ha realizado un pago de {0} euros en efectivo.",precio);
+            }
+
+            Console.WriteLine("Gracias por su compra...");
+
+            Console.ReadKey();
+        }
+    }
+}
